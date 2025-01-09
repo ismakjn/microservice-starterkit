3579d317129988367187b30777805bebab172300
ismakjn <ismakjn@gmail.com>
2025-01-09 19:54:37 +0200

    Add joi

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import {routes} from "./routes/routes.js";
@@ -9,7 +8,7 @@ import {sequelize} from './config/sequelize.js'
 dotenv.config();
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 app.use(routes)
